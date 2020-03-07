4a271f7d7586551021455a0f529e6acccc27add5
Grittie <grittielars@gmail.com>
2020-03-07 13:03:28 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -7,13 +7,66 @@ $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
 try {
-$pdo = new PDO($dsn, $user, $pass, $options);
-echo($dsn);
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    #echo($dsn);
 } catch (\PDOException $e) {
-throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+
+$titles = $pdo->query('SELECT title FROM series');
+$rating = $pdo->query('SELECT rating FROM series');
+
+echo "
+<h1>Netland film review</h1>
+<h2>Series</h2>
+<table>
+    <tr>
+        <th>Title</th>
+        <th>Rating</th>
+    </tr>";
+
+foreach ($titles as $row) {
+    echo "<tr>";
+    echo "<td>" . $row['title'] . "</td>";
+
+    foreach ($rating as $rowa) {
+        echo "<td>" . $rowa['rating'] . "</td>";
+        break;
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+$titles = $pdo->query('SELECT title FROM movies');
+$duur = $pdo->query('SELECT duur FROM movies');
+
+echo "
+<h2>Movies</h2>
+<table>
+    <tr>
+        <th>Titel</th>
+        <th>Duration</th>
+    </tr>";
+
+foreach ($titles as $row) {
+    echo "<tr>";
+    echo "<td>" . $row['title'] . "</td>";
+    foreach ($duur as $rowa) {
+        echo "<td>" . $rowa['duur'] . "</td>";
+        break;
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+</body>
+
+</html>
